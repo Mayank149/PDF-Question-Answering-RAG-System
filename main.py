9337c96e53852a3340bb7c0dbae22f8ad49f64e2
Mayank149 <bansalmayank1414@gmail.com>
2026-01-01 12:00:01 +0530

    Added genai embedding

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from pdf_text_extracter import load_pdf_text
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+import google.generativeai as genai
+import numpy as np
+import os
 
+genai.configure(api_key = os.getenv("API_KEY"))
 
 def chunk_text(text):
     splitter = RecursiveCharacterTextSplitter(
@@ -21,6 +25,17 @@ def create_chunks_with_metadata(chunks, source):
             "source" : source
         })
     return chunk_data
+
+def embed_texts(texts):
+    embeddings = []
+    for text in texts:
+        response = genai.embed_content(
+            model = "models/embedding-001",
+            content = text,
+            task_type = "retreival_document" 
+        )
+        embeddings.append(response["embedding"])
+    return np.array(embeddings).astype("float32")
 
 if __name__ == "__main__":
     text = load_pdf_text("sample.pdf")
