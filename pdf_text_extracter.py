f2395267fe3b8db14e680be3f3c37524c243bce1
Mayank149 <bansalmayank1414@gmail.com>
2026-01-01 10:55:31 +0530

    made the pdf text extracter

---
+++ pdf_text_extracter.py
@@ -0,0 +1,12 @@
+from pypdf import PdfReader
+
+def load_pdf_text(path):
+    reader = PdfReader(path)
+    full_text = ""
+
+    for page in reader.pages:
+        text = page.extract_text()
+        if text:
+            full_text += text + "\n"
+    
+    return full_text
